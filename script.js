0d17e5e87a283b15ba5bc3468aa7dd575e80793a
Brunoyam <>
2020-08-26 20:37:54 +0300

    edit tabs and spaces

--- script.js
+++ script.js
@@ -9,18 +9,19 @@ function fromLowToHigh(a, b) {
       }
       return 0;
     }
+
     GET_COLUMNS_RESPONSE.sort(fromLowToHigh);
 
-for (let i = 0; i < GET_COLUMNS_RESPONSE.length; i++) {
+    for (let i = 0; i < GET_COLUMNS_RESPONSE.length; i++) {
 
-    colName = GET_COLUMNS_RESPONSE[i].name;
+        colName = GET_COLUMNS_RESPONSE[i].name;
 
-    let newCol = document.createElement('div');
-    newCol.innerText = colName;
-    newCol.classList.add('col');
-    document.querySelector('.app').appendChild(newCol);
+        let newCol = document.createElement('div');
+        newCol.innerText = colName;
+        newCol.classList.add('col');
+        document.querySelector('.app').appendChild(newCol);
 
-}
+    }
 
     
 
